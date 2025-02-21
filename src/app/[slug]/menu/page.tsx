ff7de7c8c786597b52e2f7cd7b4e9dcfc894a23c
Leandro Rezende Albanez <leandrorezende89@gmail.com>
2025-02-21 17:09:24 -0300

    feat: add products and categories pages

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -26,7 +27,10 @@ const RestaurantMenuPage = async ({
     return notFound();
   }
 
-  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: { menuCategories: { include: { products: true } } },
+  });
 
   if (!restaurant) {
     return notFound();
@@ -35,6 +39,7 @@ const RestaurantMenuPage = async ({
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
     </div>
   );
 };

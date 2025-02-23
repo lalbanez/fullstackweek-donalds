7441124ec47cc4dbfe039fe53702b54b182a29b0
Leandro Rezende Albanez <leandrorezende89@gmail.com>
2025-02-23 17:45:37 -0300

    feat: add product details page

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { formatCurrency } from "@/helpers/format-currency";
+
 interface ProductsProps {
     products: Product[];
 }
@@ -22,10 +24,7 @@ const { slug } = useParams<{slug: string}>;
                         {product.description}
                     </p>
                     <p className="pt-3 text-sm font-semibold">
-                        {new Intl.NumberFormat("pt-BR", {
-                            style: "currency",
-                            currency: "BRL"
-                        }).format(product.price)}
+                        {formatCurrency(product.price)}
                     </p>
                 </div>
 

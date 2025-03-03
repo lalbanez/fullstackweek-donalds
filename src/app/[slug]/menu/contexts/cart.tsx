564628baeccbb6867fa31b3e8a2e840754cf7adc
Leandro Rezende Albanez <leandrorezende89@gmail.com>
2025-03-03 15:59:17 -0300

    feat: ensure cart product quantity can be decreaded

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -4,7 +4,7 @@ import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
 
-interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+export interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
     quantity: number;
 }
 
@@ -13,6 +13,7 @@ export interface ICartContext {
     products: CartProduct[];
     toggleCart: () => void;
     addProduct: (product: CartProduct) => void;
+    decreaseProductQuantity(productId: string): void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -20,6 +21,7 @@ export const CartContext = createContext<ICartContext>({
     products: [],
     toggleCart: () => { },
     addProduct: () => { },
+    decreaseProductQuantity: () => { },
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -50,12 +52,29 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
 
     };
 
+    const decreaseProductQuantity = (productId: string) => {
+        setProducts(prevProducts =>{
+            return prevProducts.map(prevProduct => {
+                if(prevProduct.id !== productId) {
+                    return prevProduct;
+                }
+
+                if(prevProduct.quantity === 1){
+                    return prevProduct;
+                }
+                
+                return {...prevProduct, quantity: prevProduct.quantity - 1}
+            })
+        })
+    }
+
     return (
         <CartContext.Provider value={{
             isOpen,
             products,
             toggleCart,
             addProduct,
+            decreaseProductQuantity,
         }}>
             {children}
         </CartContext.Provider>
